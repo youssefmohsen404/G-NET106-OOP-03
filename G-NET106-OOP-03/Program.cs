2847fec51f7fecf282930b02c9c7d6bf4e844757
youss <youssmohss111@gmail.com>
2026-09-19 16:24:04 +0300

    part1 - Q1

--- G-NET106-OOP-03/Program.cs
+++ G-NET106-OOP-03/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
+            #region part1
+            #region Q1
+            //Overloading, Overriding, and Binding
+            // a)  What is the difference between Method Overloading and Method Overriding?
 
+            // method overloading: is using the same function with it's name but changing in it's type
+            // or parameters type or parameter arrangment or number of parameters
+            // and it is a static polymorphism
+
+            //method overriding : using a function inherited from the parent class in the derived class
+            // by making the parent function virtual and using a override keyword in the new function
+            // in the derived class then changing in fucntions's body in the derived class 
+            // and method overriding is a dynamic polymorphism  
+
+            //b)  What is the difference between Static Binding and Dynamic Binding?
+            // static binding : is resolved in compile time a the compiler decide which function should be called
+            // and it's depending on the reference parent type not the object type
+
+            //dynamic binding : same as static but it's deciding which function should be called based on object type 
+            //not the reference parent and the static is faster in excution (virtual , overriding )
+
+
+
+            #endregion
+            #endregion
         }
     }
 }
